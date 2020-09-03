127c7da238c65e8f8c234eb45eea208d92edc0f4
Vien <onthewey@163.com>
2020-09-03 11:09:43 +0800

    通过sql查询管理爬虫

--- cmd/scrapy_cmd.py
+++ cmd/scrapy_cmd.py
@@ -19,39 +19,39 @@ runner = CrawlerRunner(get_project_settings())
 
 @defer.inlineCallbacks
 def crawl():
-    # while True:
-    #     # logging.info("new cycle starting")
-    #     # #yield runner.crawl("edu_info")
-    #     # yield runner.crawl("gfjyb_edu_info")
-    #     # 查询启用状态的数据源
-    #     results = MysqlDB.querystart(0)
-    #     for row in results:
-    #         origin_id = row[0]
-    #         scrapy_name = row[1]
-    #         url = row[2]
-    #         # 打印结果
-    #         logging.info("origin_id=%s,scrapy_name=%s,url=%s" % \
-    #                      (origin_id, scrapy_name, url))
-    #         logging.info("new cycle starting")
-    #         #origin_id 数据源id, scrapy_name 爬虫脚本名称, url 爬虫地址url
-    #         yield runner.crawl(crawler_or_spidercls=scrapy_name, oderurl=url, origin_id=origin_id)
+    while True:
+        # logging.info("new cycle starting")
+        # #yield runner.crawl("edu_info")
+        # yield runner.crawl("gfjyb_edu_info")
+        # 查询启用状态的数据源
+        results = MysqlDB.querystart(0)
+        for row in results:
+            origin_id = row[0]
+            scrapy_name = row[1]
+            url = row[2]
+            # 打印结果
+            logging.info("origin_id=%s,scrapy_name=%s,url=%s" % \
+                         (origin_id, scrapy_name, url))
+            logging.info("new cycle starting")
+            #origin_id 数据源id, scrapy_name 爬虫脚本名称, url 爬虫地址url
+            yield runner.crawl(crawler_or_spidercls=scrapy_name, oderurl=url, origin_id=origin_id)
+
+         # 1s跑一次
+        time.sleep(5)
+    reactor.stop()
+
     #
-    #      # 1s跑一次
-    #     time.sleep(5)
-    # reactor.stop()
-
-
-    results = MysqlDB.querystart(0)
-    for row in results:
-        origin_id = row[0]
-        scrapy_name = row[1]
-        url = row[2]
-        # 打印结果
-        logging.info("origin_id=%s,scrapy_name=%s,url=%s" % \
-                     (origin_id, scrapy_name, url))
-        logging.info("new cycle starting")
-        # origin_id 数据源id, scrapy_name 爬虫脚本名称, url 爬虫地址url
-        yield runner.crawl(crawler_or_spidercls=scrapy_name, oderurl=url, origin_id=origin_id)
+    # results = MysqlDB.querystart(0)
+    # for row in results:
+    #     origin_id = row[0]
+    #     scrapy_name = row[1]
+    #     url = row[2]
+    #     # 打印结果
+    #     logging.info("origin_id=%s,scrapy_name=%s,url=%s" % \
+    #                  (origin_id, scrapy_name, url))
+    #     logging.info("new cycle starting")
+    #     # origin_id 数据源id, scrapy_name 爬虫脚本名称, url 爬虫地址url
+    #     yield runner.crawl(crawler_or_spidercls=scrapy_name, oderurl=url, origin_id=origin_id)
 
 
 crawl()
